a4041a4df86b22c8a524c7024d64179b33ebd1f2
James <13262021+james-coder@users.noreply.github.com>
2023-04-05 21:44:29 -0600

    Update controller.py
    Since we removed the driver parameter from the GoogleCloudSQL class, it should be removed from the controller.py file as well. Additionally, the encrypt parameter is not required for the pymysql connection, so it can also be removed from the configuration and GoogleCloudSQL instantiation.

--- controller.py
+++ controller.py
@@ -2,18 +2,20 @@ import json
 from chatgpt import ChatGPT
 from google_sql_connector import GoogleCloudSQL
 import configparser
+import logging
 
-# Read the config file
-config = configparser.ConfigParser()
-config.read("config.ini")
+def read_config():
+    config = configparser.ConfigParser()
+    config.read("config.ini")
+    return config
+
+config = read_config()
 
 # Access the config values
-driver = config.get("database", "driver")
 server = config.get("database", "server")
 database = config.get("database", "database")
 user = config.get("database", "user")
 password = config.get("database", "password")
-encrypt = config.get("database", "encrypt")
 openai_api_key = config.get("openai", "api_key")
 openai_org = config.get("openai", "org")
 openai_model = config.get("openai", "model")
@@ -23,17 +25,17 @@ class Controller:
 
     def __init__(self):
         # initialise all the things
-        self.google_sql = GoogleCloudSQL(driver, server, database, user, password, encrypt)
+        self.google_sql = GoogleCloudSQL(server, database, user, password)
         self.google_sql.connect()
         self.chatModel = ChatGPT(openai_api_key, openai_org, openai_model)
 
     def run(self, message, sender, counter=0):
-        if (counter > 4):
+        if counter > 4:
             return 'error: too many requests'
-        responseString = self.chatModel.message(message, sender)
+        response_string = self.chatModel.message(message, sender)
         try:
-            response = json.loads(responseString[:-1] if responseString.endswith('.') else responseString)
-        except ValueError:
+            response = json.loads(response_string[:-1] if response_string.endswith('.') else response_string)
+        except json.JSONDecodeError:
             return self.run("Please repeat that answer but use valid JSON only.", "SYSTEM", counter + 1)
         match response["recipient"]:
             case "USER": 
@@ -47,8 +49,8 @@ class Controller:
                         result = self.google_sql.execute_schema(response["message"])
                         return self.run(result, None, counter + 1)
                     case _:
-                        print('error invalid action')
-                        print(response)
+                        logging.error('Invalid action')
+                        logging.error(response)
             case _:
-                print('error, invalid recipient')
-                print(response)
+                logging.error('Invalid recipient')
+                logging.error(response)
